463172f8907b72d51028645f7e2145f4d4fa0e57
Lisa K <lkalych@gmail.com>
2020-07-26 15:50:17 -0600

    for loop example

--- Program.cs
+++ Program.cs
@@ -11,74 +11,10 @@ namespace SoloLearn
     {
         static void Main(string[] args)
         {
-            /*
-            namespace SoloLearn
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int x = 1;
-while (x++ < 5)
-{
-    if (x % 2 == 0)
-        x += 2;
-}
-
-
-        }
-    }
-}
-*/
-
-            int x = 1, loopCount = 0;
-            Console.WriteLine("while loop");
-            while (x++ < 5)
+            for (int x = 10; x < 15; x++)
             {
-                ++loopCount;
-                Console.WriteLine(
-                    $"Loop {loopCount}; x = {x}"
-                );
-                if (x % 2 == 0)
-                {
-                    x += 2;
-                    Console.WriteLine(
-                        $"Added 2 to x: {x}"
-                    );
-                }
+                Console.WriteLine("Value of x: {0}", x);
             }
-            Console.WriteLine(
-                $"Program looped {loopCount} times"
-            );
-            Console.WriteLine(
-                $"x = {x}"
-            );
-            
-            x = 1;
-            loopCount = 0;
-            
-            Console.WriteLine();
-            Console.WriteLine("for loop");
-            for ( ; x++ < 5; )
-            {
-                ++loopCount;
-                Console.WriteLine(
-                    $"Loop {loopCount}; x = {x}"
-                );
-                if (x % 2 == 0)
-                {
-                    x += 2;
-                    Console.WriteLine(
-                        $"Added 2 to x: {x}"
-                    );
-                }
-            }
-            Console.WriteLine(
-                $"Program looped {loopCount} times"
-            );
-            Console.WriteLine(
-                $"x = {x}"
-            );
         }
     }
 }
