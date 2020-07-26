f23012136dbb63e88fbf2734c98a487930e38e6f
Lisa K <lkalych@gmail.com>
2020-07-26 14:49:03 -0600

    step by step explanation of loop

--- Program.cs
+++ Program.cs
@@ -11,9 +11,74 @@ namespace SoloLearn
     {
         static void Main(string[] args)
         {
-            int num = 0;
-            while(num ++ < 6) 
-                Console.WriteLine(num);
+            /*
+            namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+while (x++ < 5)
+{
+    if (x % 2 == 0)
+        x += 2;
+}
+
+
+        }
+    }
+}
+*/
+
+            int x = 1, loopCount = 0;
+            Console.WriteLine("while loop");
+            while (x++ < 5)
+            {
+                ++loopCount;
+                Console.WriteLine(
+                    $"Loop {loopCount}; x = {x}"
+                );
+                if (x % 2 == 0)
+                {
+                    x += 2;
+                    Console.WriteLine(
+                        $"Added 2 to x: {x}"
+                    );
+                }
+            }
+            Console.WriteLine(
+                $"Program looped {loopCount} times"
+            );
+            Console.WriteLine(
+                $"x = {x}"
+            );
+            
+            x = 1;
+            loopCount = 0;
+            
+            Console.WriteLine();
+            Console.WriteLine("for loop");
+            for ( ; x++ < 5; )
+            {
+                ++loopCount;
+                Console.WriteLine(
+                    $"Loop {loopCount}; x = {x}"
+                );
+                if (x % 2 == 0)
+                {
+                    x += 2;
+                    Console.WriteLine(
+                        $"Added 2 to x: {x}"
+                    );
+                }
+            }
+            Console.WriteLine(
+                $"Program looped {loopCount} times"
+            );
+            Console.WriteLine(
+                $"x = {x}"
+            );
         }
     }
 }
