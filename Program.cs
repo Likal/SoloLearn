82107ee421bae6df02527d0553007f80fa67d8ba
Lisa K <lkalych@gmail.com>
2020-07-26 14:35:05 -0600

    shortened while loop increment inside condition

--- Program.cs
+++ Program.cs
@@ -7,22 +7,13 @@ using System.Threading.Tasks;
 
 namespace SoloLearn
 {
-    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
     class Program
     {
         static void Main(string[] args)
         {
-            int num = 1;
-            while(num < 6) 
-            {
+            int num = 0;
+            while(++num < 6) 
                 Console.WriteLine(num);
-                num+=2;
-            }
-        }
-
-        private string GetDebuggerDisplay()
-        {
-            return ToString();
         }
     }
 }
