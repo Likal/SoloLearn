a18a2c2514922c5220d6c871ab3c0cf5b61a8755
Lisa K <lkalych@gmail.com>
2020-08-02 14:04:22 -0600

    error without static Main method included

--- Program.cs
+++ Program.cs
@@ -10,25 +10,8 @@ namespace SoloLearn
 
     class Program
     {
-        static void Main(string[] args)
-        {
-        
-        // This code will still cause an unhandled exception if upper case EXIT is used, or any other letter or word
-        do {
-         Console.Write("x = ");
-         string str = Console.ReadLine();
-         if(str == "exit")
-         break;
-
-         int x = Convert.ToInt32(str);
-
-         Console.Write("y = ");
-         int y = Convert.ToInt32(Console.ReadLine());
-
-         int sum = x + y;
-         Console.WriteLine("Result: {0}", sum);
-        }
-        while(true);
+        static void SayHi(){
+            Console.WriteLine("Hello");
         }
     }
 }
